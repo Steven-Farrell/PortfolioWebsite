6a67fb5032256fdab438c3c9452b887c54c283be
Steven Farrell <steven.farrellx@gmail.com>
2023-02-12 18:27:44 -0500

    This is some stuff please hire me

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import React from "react";
+import Navbar from "./components/navbar";
+import AboutThisWebsite from "./components/aboutus";
+import ToDoText from "./todotext";
+import CalculatorText from "./calculatortext";
+import FormText from "./formtext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <AboutThisWebsite />
+      <ToDoText />
+      <CalculatorText />
+      <FormText />
     </div>
   );
 }
